1f79ef89d07b86995630b64ea000d0be90a58fda
Kraix <2krai2x@gmail.com>
2011-11-11 15:51:10 +0100

    Update hra.pas

--- hra.pas
+++ hra.pas
@@ -3,10 +3,11 @@ uses Graph, Crt;
 var Gd,Gm:Integer;
 begin
    DetectGraph(Gd,Gm);
-   InitGraph(Gd,Gm,’ ’);
+   InitGraph(Gd,Gm,' ');
    SetColor(10);
-   SetFillStyle(8,10);
-   Bar(10,400,500,480);
+   SetFillStyle(1,10);
+   Bar(1,1024,100,900);
+
     repeat until KeyPressed;
    CloseGraph;
 end.
