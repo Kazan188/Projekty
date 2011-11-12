d5e1b42c6f21e05afe077ccf32ddc0e8c4b391b3
Kraix <2krai2x@gmail.com>
2011-11-12 10:18:26 +0100

    Update hra.pas

--- hra.pas
+++ hra.pas
@@ -1,13 +1,6 @@
 program nemesis;
-uses Graph, Crt;
-var Gd,Gm:Integer;
+uses Crt;
+var 
 begin
-   DetectGraph(Gd,Gm);
-   InitGraph(Gd,Gm,' ');
-   SetColor(10);
-   SetFillStyle(1,10);
-   Bar(1,1024,100,900);
-
-    repeat until KeyPressed;
-   CloseGraph;
+   
 end.
